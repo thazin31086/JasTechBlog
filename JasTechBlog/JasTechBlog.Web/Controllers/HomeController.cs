316468d8be8d708531f3f21b1d06e895775552b5
Jasmine Aung <thazin31086@gmail.com>
2017-01-30 16:22:16 +1100

    Fix Reference issue

--- JasTechBlog/JasTechBlog.Web/Controllers/HomeController.cs
+++ JasTechBlog/JasTechBlog.Web/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace JasTechBlog.Web.Controllers
 {
@@ -14,16 +10,13 @@ namespace JasTechBlog.Web.Controllers
         }
 
         public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
+        {          
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
